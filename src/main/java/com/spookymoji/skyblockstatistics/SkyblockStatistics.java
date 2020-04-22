aba5e01f39b17dffeea8130d3cacaa6496099504
Marco Rossi <marco.rossi1337@gmail.com>
2020-04-22 13:43:59 +0200

    Woops! Updated versions

--- src/main/java/com/spookymoji/skyblockstatistics/SkyblockStatistics.java
+++ src/main/java/com/spookymoji/skyblockstatistics/SkyblockStatistics.java
@@ -19,7 +19,7 @@ public class SkyblockStatistics
 {
     public static final String MODID = "skyblockstatistics";
     public static final String MOD_NAME = "SkyblockStatistics";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1.0";
 
     public static String SB_API_KEY = "";
     
